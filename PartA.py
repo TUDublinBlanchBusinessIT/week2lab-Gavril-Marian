7ffa079dd03696f9cfbaf61a7418aa5b6fda3fb6
B00165945 Gavril Marian <B00165945@mytudublin.ie>
2025-02-10 17:38:22 +0000

    Completed Part A

---
+++ PartA.py
@@ -0,0 +1,16 @@
+#PartA.py
+#A Program to Print Steps 1 to 9 to the Screen
+#Gavril Marian B00165945
+#10/02/2025
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
